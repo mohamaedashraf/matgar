33d3fdf876c2bf0b4ca0c6675f73259a0ea25e04
mohamaedashraf <35531522+mohamaedashraf@users.noreply.github.com>
2018-01-17 16:13:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//ar" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html  xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>متجرى</title>
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<meta name="description" content="Simple Multi-Item Slider: Category slider with CSS animations" />
+		<meta name="keywords" content="jquery plugin, item slider, categories, apple slider, css animation" />
+		<meta name="author" content="Codrops" />
+		<link rel="stylesheet" type="text/css" href="css/slider.css" />
+		<script src="js/modernizr.custom.63321.js"></script>
+</head>
+
+<body>
+     <div class="topnav">
+     <div id="infonav">
+ 	     <ul>
+		<li><a href="www.google.com">تسجيل الدخول</a></li>
+		<li><a href="www.facebook.com"> الاستفسار و المساعده </a></li>
+		 </ul> 
+     </div>
+     <div class="loginfo">
+       <ul>
+         <li><a href="#">تسجيل الدخول</a></li>
+         <li><a href="#">التسجيل في المتجر</a></li>                 
+       </ul>
+     </div>    
+	 </div>
+	 <header>
+	  <div id="headers">
+                <div id="logo">
+                    <a href="index.php"><img src="css/images/logo.png" alt="logo" ></a>
+                </div><!-- end logo -->
+<div id="sale" dir="rtl" style="display:inline-block;">
+<img src="images/shopping-cart.png" width="20" height="20" alt="logo"> عربة الشراء</a>
+</div><!--end sale-->
+            </div><!-- end headers -->
+        </header>
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<style media="screen" type="text/css"> 
+<!--
+#wrapper   { margin: 0px auto;width: 536px; }
+.content-h { background:#fafafa;padding: 10px;height:150px;width:580px; }
+.content-v { background:#fafafa;padding: 10px;height:150px;width:390px;float:left; }
+p.by       { border-top: 1px solid #C8C8C8;margin:25px 0 0px;padding-top: 5px;font-size: 15px; clear:both; }
+-->
+</style>
+
+
+ 
+</head>
+<body>
+<div id="navigation">
+ <ul>
+   <li><a href="#">القوالب</a></li>
+   <li><a href="#">فيكتور</a></li>
+   <li><a href="#">سكريبتات</a></li>
+   <li><a href="#">سكريبتات</a></li> 
+   <li><a href="#">سكريبتات</a></li>    
+ </ul>
+</div>
+<br></br>
+<div class="container">	
+			<!-- Codrops top bar -->
+			</header>
+			<div class="main">
+				<div id="mi-slider" class="mi-slider" dir="rtl">
+					<ul>
+						<li><a href="#"><img src="images/1.jpg" alt="ووردبريس"><h4>وورد بريس</h4></a></li>
+						<li><a href="#"><img src="images/2.jpg" alt="img02"><h4>جوملا</h4></a></li>
+						<li><a href="#"><img src="images/3.jpg" alt="img03"><h4>Loafers</h4></a></li>
+						<li><a href="#"><img src="images/4.jpg" alt="img04"><h4>Sneakers</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/5.jpg" alt="img05"><h4>Belts</h4></a></li>
+						<li><a href="#"><img src="images/6.jpg" alt="img06"><h4>Hats &amp; Caps</h4></a></li>
+						<li><a href="#"><img src="images/7.jpg" alt="img07"><h4>Sunglasses</h4></a></li>
+						<li><a href="#"><img src="images/8.jpg" alt="img08"><h4>Scarves</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/9.jpg" alt="img09"><h4>Casual</h4></a></li>
+						<li><a href="#"><img src="images/10.jpg" alt="img10"><h4>Luxury</h4></a></li>
+						<li><a href="#"><img src="images/11.jpg" alt="img11"><h4>Sport</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
+						<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
+						<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
+						<li><a href="#"><img src="images/15.jpg" alt="img15"><h4>Briefcases</h4></a></li>
+					</ul>
+                    <ul>
+						<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
+						<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
+						<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
+						<li><a href="#"><img src="images/15.jpg" alt="img15"><h4>Briefcases</h4></a></li>
+					</ul>
+					<nav>
+						<a href="#">قوالب</a>
+						<a href="#">تصميمات</a>
+						<a href="#">فيكتورز</a>
+						<a href="#">سكريبتات</a>
+                        <a href="#">مداخل فيديو</a>
+					</nav>
+				</div>
+			</div>
+		</div><!-- /container -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+		<script src="js/jquery.catslider.js"></script>
+		<script>
+			$(function() {
+
+				$( '#mi-slider' ).catslider();
+
+			});
+		</script>
+<p align="center"><a href="#" class="button1">ابدا من هنا</a></p>
+</div>
+</div>
+<br></br>
+
+<h4><p align="center">نعرض لكم القوالب الاشهر والأكثر مبيعا فى موقعنا</p></h4><br />
+<p align="center"><a href="#" class="link">لمشاهدة كافة القوالب من هنا</a></p>
+<div id="content"><br /><br />
+<h1><P DIR="rtl" ALIGN="center">القوالب الاكثر مبيعا</p></h1>
+<h3><P DIR="rtl" ALIGN="right" style="color:#c10008;"> > وورد بريس</P></h1>
+<hr /><br /><br />
+<div class="iconbox">
+<ul>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+</ul>
+</div>
+<p align="center"><a href="#" class="link1">مشاهدة المزيد</a></p>
+
+<br /><hr />
+ <h1> <P dir="rtl" ALIGN="right" style="color:#c10008;"> > جوملا </P></h1>
+<div class="iconbox">
+<ul>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+</ul>
+</div>
+<p align="center"><a href="#" class="link1">مشاهدة المزيد</a></p>
+<h3><P DIR="rtl" ALIGN="right"><font>مداخل الفيديو</font></p></h3>
+<br></br>
+<div class="iconbox">
+<ul>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+<li><a href="#"><IMG SRC="css/images/mtgar_15-32.GIF"></a></li>
+</ul>
+</div>
+<p align="center"><a href="#" class="link1">مشاهدة المزيد</a></p>
+</div>
+<div id="footer">  </div>
